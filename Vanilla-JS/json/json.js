e15348bfbde304555f6ab1bbab1db0bace0d0acd
satanvir5 <satanvir5@gmail.com>
2019-09-19 03:13:00 +0600

    json

--- Vanilla-JS/json/json.js
+++ Vanilla-JS/json/json.js
@@ -1,50 +1,46 @@
-// //Array
-// let teacher = [];
-// const student = ["farjana", "Tuli", "Akter", "Farhana", "Tanvir"];
+//Array
+let teacher = [];
+const student = ["farjana", "Tuli", "Akter", "Farhana", "Tanvir"];
 
-// console.log(student);
-// //alert(student[3])
+console.log(student);
+//alert(student[3])
 
-// student[1] = "farha";
-// console.log(student);
+student[1] = "farha";
+console.log(student);
 
-// //Object
+//Object
 
-// const Tuli = {
-//     Name: "Farjana Akter Tuli",
-//     Gender: "female",
-//     Study: {
-//         org: ["IIUC", "SGHS", "Port city"],
-//         subject: "B.Sc in CSE at IIUC"
-//     },
-//     Age: 22
-// };
+const Tuli = {
+  Name: "Farjana Akter Tuli",
+  Gender: "female",
+  Study: {
+    org: ["IIUC", "SGHS", "Port city"],
+    subject: "B.Sc in CSE at IIUC"
+  },
+  Age: 22
+};
 
-// console.log(Tuli);
-// console.log(Tuli.Study.subject);
-// console.log(Tuli.Study.org[2]);
+console.log(Tuli);
+console.log(Tuli.Study.subject);
+console.log(Tuli.Study.org[2]);
 
-// //LOOP:for Array
-// //show position by using in
-// for (let index in student) {
+//LOOP:for Array
+//show position by using in
+for (let index in student) {
+  console.log(index);
+  console.log(student[index]);
+}
+//show position value by using of:
+for (let item of student) {
+  console.log(item);
+}
 
-//    console.log(index);
-//    console.log(student[index])
-
-// }
-//  //show position value by using of:
-// for (let item of student) {
-
-//     console.log(item);
-
-// }
-
-// //LOOP:for Object
-// //show position by using in:
-// for (let property in Tuli) {
-//     console.log(property)
-//     console.log(Tuli[property]);
-// }
+//LOOP:for Object
+//show position by using in:
+for (let property in Tuli) {
+  console.log(property);
+  console.log(Tuli[property]);
+}
 
 let StudentDatabase = [
   {
@@ -189,3 +185,144 @@ let Doctor = [
 console.log(Doctor);
 console.log(Doctor[1].Patient_id.mobile_Number[0]);
 console.log(Doctor[1].Patient_id.mobile_Number[1]);
+
+//Mobile Phone
+
+let Address = {
+  Home: "Najir Ahmad Master Bari",
+  Village: "Norsinghapur",
+  Post_Office: "Kabir Hat",
+  Thana: "Kabir Hat",
+  District: "Noakhali"
+};
+
+let mobile = [
+  {
+    iphone: {
+      model: "iPhone 5",
+      display: 5,
+      price: 30000,
+      ram: 2,
+      rom: 16
+    }
+  },
+  {
+    samsung: [
+      {
+        A70: {
+          model: "Samsang A70",
+          display: 6.7,
+          price: 38900,
+          ram: 6,
+          rom: 128
+        }
+      },
+      {
+        A50: {
+          model: "Samsang A50",
+          display: 6.3,
+          price: 25000,
+          ram: 4,
+          rom: 128
+        }
+      },
+      {
+        A20: {
+          model: "Samsang A20",
+          display: 6.3,
+          price: 15990,
+          ram: 3,
+          rom: 64
+        }
+      }
+    ]
+  }
+];
+console.log(mobile[1].samsung[0]);
+
+let room = [
+  "bed",
+  table = ["computer table", "normal table"],
+  "chair",
+  "hanger",
+  "cloth",
+  "mat",
+  "window",
+  laptop={
+    model: "Lenevo ideapad 320",
+    "graphics card": "Nvidia GEFORCE",
+    ram: "8 GB",
+    "Hard disk": "2 TB",
+    core: "Core i5",
+    generation: "8th"
+  }
+  
+];
+
+console.log(room[1][1]);
+console.log(room[7]["graphics card"]);
+
+let cricket_team = [
+  {
+    Bangladesh: [
+      "Tamim Ikbal",
+      "Soumya Sarker",
+      "Najmul Hossain Shanto",
+      "Liton Das",
+      "Shakib Al Hasan (c)",
+      "Mushfiqur Rahim (wk)",
+      "Mahmudullah",
+      "Mosaddek Hossain",
+      "Afif Hossain",
+      "Aminul Islam",
+      " Mohammad Saifuddin",
+      "Shafiul Islam",
+      "Mustafizur Rahman",
+      "Sabbir Rahman",
+      "Taijul Islam",
+      "Rubel Hossain",
+      "Mohammad Naim"
+    ]
+  },
+  {
+    India: [
+      "Rohit Sharma",
+      "Shikhar Dhawan",
+      "Virat Kohli (c)",
+      "Shreyas Iyer",
+      "Rishabh Pant (wk)",
+      "Hardik Pandya",
+      "Krunal Pandya",
+      "Ravindra Jadeja",
+      "Washington Sundar",
+      "Deepak Chahar",
+      "Navdeep Saini",
+      "Lokesh Rahul",
+      "Manish Pandey",
+      "Rahul Chahar",
+      "K Khaleel Ahmed"
+    ]
+  },
+  {
+    "South Africa": [
+      "Quinton de Kock (c & wk)",
+      "Reeza Hendricks",
+      "Temba Bavuma",
+      "Rassie van der Dussen",
+      "David Miller",
+      "Andile Phehlukwayo",
+      "Dwaine Pretorius",
+      "Bjorn Fortuin",
+      "Kagiso Rabada",
+      "Anrich Nortje",
+      "Tabraiz Shamsi",
+      "George Linde",
+      "Junior Dala",
+      "Beuran Hendricks"
+    ]
+  }
+];
+console.log(cricket_team[0]);
+console.log(cricket_team[1].India[5]);
+
+
